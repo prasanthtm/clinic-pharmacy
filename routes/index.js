c5569a6473331a567b3c23f704092aa362ea8534
NITHIL A <97227643+Nithil-A@users.noreply.github.com>
2023-03-09 16:40:10 +0530

    Delete routes directory

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const homeController = require('../controllers/index');
-
-/* GET home page. */
-router.get('/', homeController.homeResponce);
-
-module.exports = router;
