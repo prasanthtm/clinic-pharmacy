02d43363b81d6dea2a42863edff44de37d18e834
shaheer <shanshaheer3@gmail.com>
2023-03-14 14:06:01 +0530

    Time zone

--- helper/helper.js
+++ helper/helper.js
@@ -1,10 +1,9 @@
 const validator = require('email-validator');  // Importing the email-validator library and assigning it to validator variable
 const { v4: uuidv4 } = require('uuid'); // import uuid module for generating unique identifier   
-const multer = require('multer');
 const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();  //validates international phone numbers
 const { getCode } = require('country-list');
-
-
+const ct = require('countries-and-timezones');
+const moment = require('moment-timezone');
 
 module.exports = class helper
 {
@@ -13,7 +12,7 @@ module.exports = class helper
         return new Promise((resolve,reject)=>
         {
             //For getting country abbriviation
-            const abb =getCode(country)
+            const abb = getCode (country)
             // This is regex or regular expression for verify the contact number validation
             const phoneNumberRegex = new RegExp(/^[0-9+ \s \+ \( \) \-]+$/);
             const contactNO= phoneUtil.isValidNumberForRegion(phoneUtil.parse(phoneNumber, `${abb}`), `${abb}`);
@@ -33,5 +32,30 @@ module.exports = class helper
                 reject(false)
             }
         });
-    }
+    };
+
+    static timezone()
+    {
+        return new Promise((resolve,reject)=>
+        {
+            console.log("Inside");     
+            const abb = getCode("nepal")
+            console.log(abb);
+            const country = ct.getCountry(abb);
+            console.log(country.timezones[0]);
+            // Create a Moment.js object representing the current date and time in the Pacific/Honolulu time zone
+            const DateTime = moment.tz(country.timezones[0]);
+            // Display the current date and time in the Pacific/Honolulu time zone
+            let time = DateTime.format('YYYY-MM-DD HH:mm:ss')
+            console.log(time);
+
+
+        });
+    };
+
+
+
+
+
+
 }
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const helper = require('../helper/helper'); 
 
-
+helper.timezone();
 
 /* GET users listing. */
-router.get('/', (req, res, next)=> 
-{
-  helper.emailValidation('abc@gmail.com')
-});
+// router.get('/', (req, res, next)=> 
+// {
+//   helper.emailValidation('abc@gmail.com')
+// });
 
 
 
